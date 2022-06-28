56047a0976255c963f1f3a6ff48c858c704266f6
widiskel <waputra16@gmail.com>
2022-06-28 15:36:10 +0700

    update

--- lib/app/modules/landing_page/views/landing_page_view.dart
+++ lib/app/modules/landing_page/views/landing_page_view.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: prefer_const_constructors, unused_import
+// ignore_for_file: prefer_const_constructors, unused_import, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:media_pembelajaran_dan_tts/app/pallete/color_pallete.dart';
 import 'package:media_pembelajaran_dan_tts/app/routes/app_pages.dart';
 
 import '../controllers/landing_page_controller.dart';
@@ -24,10 +25,40 @@ class LandingPageView extends GetView<LandingPageController> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () => Get.offAllNamed(Routes.TTS),
-              child: Text("Mulai"),
+            GestureDetector(
+              child: Container(
+                width: 100,
+                height: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(colors: const [appRed, appWhite]),
+                ),
+                child: Center(
+                  child: Text(
+                    "Mulai",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
             ),
+            // ElevatedButton(
+            //   onPressed: () => Get.offAllNamed(Routes.TTS),
+            //   child: Text(
+            //     "Mulai",
+            //     style: TextStyle(
+            //       fontSize: 20,
+            //       fontWeight: FontWeight.bold,
+            //     ),
+            //   ),
+            //   style: ElevatedButton.styleFrom(
+            //       primary: Colors.red,
+            //       shape: RoundedRectangleBorder(
+            //           borderRadius: BorderRadius.circular(30)),
+            //       padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20)),
+            // ),
           ],
         ),
       ),
---
+++ lib/app/pallete/color_pallete.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+const appRed = Color.fromARGB(255, 255, 0, 0);
+const appWhite = Color.fromARGB(255, 255, 255, 255);
