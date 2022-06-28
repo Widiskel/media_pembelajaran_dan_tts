4ed2d00f2f9372b01f5b12eb283566f145a490fc
widiskel <waputra16@gmail.com>
2022-06-28 13:38:42 +0700

    update

--- lib/app/modules/landing_page/views/landing_page_view.dart
+++ lib/app/modules/landing_page/views/landing_page_view.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unused_import
 
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:media_pembelajaran_dan_tts/app/routes/app_pages.dart';
 
 import '../controllers/landing_page_controller.dart';
 
@@ -11,14 +12,23 @@ class LandingPageView extends GetView<LandingPageController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LandingPageView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'LandingPageView is working',
-          style: TextStyle(fontSize: 20),
+      body: Container(
+        height: Get.height,
+        width: Get.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/img/back.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => Get.offAllNamed(Routes.TTS),
+              child: Text("Mulai"),
+            ),
+          ],
         ),
       ),
     );
